e61925bcb6b5eca829040640f54e50130f0afe1d
Scotty Phillips <scotty.d.phillips@gmail.com>
2022-07-13 15:15:40 -0400

    day of the programmer php added

---
+++ php/day_of_the_programmer.php
@@ -0,0 +1,18 @@
+function day_of_the_programmer($year) {
+  if ($year == 1918) {
+    return '26.09.1918';
+  }
+  elseif ($year < 1918) {
+    if ($year % 4 == 0) {
+      return '12.09.' . $year;
+    } else {
+      return '13.09.' . $year; 
+    }
+  } else {
+    if ($year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+      return '12.09.' . $year;
+    } else {
+      return '13.09.' . $year;
+    }
+  }
+}
